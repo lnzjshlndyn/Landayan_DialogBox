24759bc9411a72383188afd609b89a067a7f8774
lnzjshlndyn <2014069168@ust-ics.mygbiz.com>
2017-09-28 14:51:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/josh/dialogbox/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.josh.dialogbox;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnAlert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAlert = (Button) findViewById(R.id.btnAlert);
+    }
+
+    public void showDialog(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Do you want to continue?")
+                .setMessage("Intrusion Detected")
+                .setCancelable(true)
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                })
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+
+        builder.create().show();
+    }
+}
